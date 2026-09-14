d9887bf09c7af38b4bd2381f609d1619d2866c2f
akarshi <f20240589@pilani.bits-pilani.ac.in>
2026-09-14 07:11:11 +0530

    removed backup

--- .backups/20260824_000000/backend_backup/middleware/adminAuth.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (req, res, next) {
-  if (!req.isAdmin) {
-    return res.status(403).json({ message: 'Admin privileges required' });
-  }
-  next();
-};
--- .backups/20260824_000000/backend_backup/utils/errorHandler.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (err, req, res, next) {
-  console.error(err && err.stack ? err.stack : err);
-  const status = err.status || 500;
-  res.status(status).json({ message: err.message || 'Internal Server Error' });
-};
